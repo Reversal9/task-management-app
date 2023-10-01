19923007ea2f91ef0796ff293f7250cb11a2ec90
jonne <jonnenc@gmail.com>
2023-09-30 23:27:48 -0500

    Add schema validation by refactoring add column UI to form

--- client/src/components/ProjectContent.tsx
+++ client/src/components/ProjectContent.tsx
@@ -5,36 +5,47 @@ import ProjectColumn from "@/components/ProjectColumn";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Check, PlusIcon, X } from "lucide-react";
-import { IColumn } from "@/types/column";
 import { shallowEqual } from "react-redux";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form.tsx";
 
 const ProjectContent: React.FC = (): React.ReactElement | undefined => {
     const dispatch = useAppDispatch();
     const columnIds: string[] = useAppSelector<string[]>(selectColumnIds, shallowEqual);
     const confirmButtonRef: React.RefObject<HTMLButtonElement> = useRef<HTMLButtonElement>(null);
     
-    //Handle add column
     const [isAddingNewColumn, setIsAddingNewColumn] = useState<boolean>(false);
     
-    const newColumnSlate: Omit<IColumn, "_id"> = {
-        title: "",
-        taskIds: []
+    const schema = z.object({
+        title: z.string().min(1).max(30)
+    });
+    
+    const form = useForm<z.infer<typeof schema>>({
+        resolver: zodResolver(schema),
+        defaultValues: {
+            title: ""
+        }
+    });
+    
+    function onSubmit(values: z.infer<typeof schema>) {
+        setIsAddingNewColumn(false);
+        dispatch(addColumn({
+            title: values.title,
+            taskIds: []
+        })).unwrap()
+            .then(() => {
+                form.reset();
+            })
+            .catch(() => {
+                setIsAddingNewColumn(true);
+            });
     }
-    const [newColumnValue, setNewColumnValue] = useState<string>("");
     
     function handleBlur() {
         setIsAddingNewColumn(false);
-        setNewColumnValue("");
-    }
-    
-    function handleAddColumn() {
-        dispatch(addColumn({
-            ...newColumnSlate,
-            title: newColumnValue
-        })).unwrap()
-            .then(() => {
-                handleBlur();
-            });
+        form.reset();
     }
     
     return (
@@ -49,39 +60,48 @@ const ProjectContent: React.FC = (): React.ReactElement | undefined => {
                     <PlusIcon size = {24} strokeWidth = {2} color = "#52525B"></PlusIcon>
                 </Button>
             :
-                <div className = "flex flex-col min-h-mc w-64 p-3 bg-gray-200/50 rounded-sm gap-1">
-                    <Input
-                        className = "h-6 text-sm text-zinc-500 font-semibold resize-none overflow-hidden bg-transparent border-none hover:bg-slate-600/10 focus:bg-white truncate"
-                        maxLength = {30}
-                        onChange = {(e: React.ChangeEvent<HTMLInputElement>) => setNewColumnValue(e.target.value)}
-                        onKeyDown = {(e: React.KeyboardEvent<HTMLInputElement>) => {
-                            if (e.key === "Escape") {
-                                e.currentTarget.blur();
-                            }
-                            if (e.key === "Enter") {
-                                handleAddColumn();
-                            }
-                        }}
-                        onBlur = {(e: React.FocusEvent<HTMLInputElement, Element>) => {
+                <Form {...form}>
+                    <form
+                        onSubmit = {form.handleSubmit(onSubmit)}
+                        className = "flex flex-col min-h-mc w-64 p-3 bg-gray-200/50 rounded-sm gap-1"
+                        onBlur = {(e) => {
                             if (e.relatedTarget !== confirmButtonRef.current) {
                                 handleBlur();
                             }
-                        }}
-                        value = {newColumnValue}
-                        spellCheck = {false}
-                        autoFocus>
-                    </Input>
-                    <div className = "flex flex-row justify-end gap-1">
-                        <Button className = "bg-gray-200/50 shadow-md rounded-sm aspect-square" ref = {confirmButtonRef} variant = "task" size = "icon" onClick ={() => {
-                            handleAddColumn();
                         }}>
-                            <Check size = {24} strokeWidth = {2} color = "#52525B"></Check>
-                        </Button>
-                        <Button className = "bg-gray-200/50 shadow-md rounded-sm aspect-square" variant = "task" size = "icon">
-                            <X size = {24} strokeWidth = {2} color = "#52525B"></X>
-                        </Button>
-                    </div>
-                </div>
+                        <FormField control = {form.control} name = "title" render = {({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input
+                                        className = "h-6 text-sm text-zinc-500 font-semibold resize-none overflow-hidden bg-transparent border-none hover:bg-slate-600/10 focus:bg-white truncate"
+                                        {...field}
+                                        maxLength = {30}
+                                        onKeyDown = {(e: React.KeyboardEvent<HTMLInputElement>) => {
+                                            if (e.key === "Escape") {
+                                                e.currentTarget.blur();
+                                            }
+                                            if (e.key === "Enter") {
+                                                form.handleSubmit(onSubmit);
+                                            }
+                                        }}
+                                        spellCheck = {false}
+                                        autoFocus>
+                                    </Input>
+                                </FormControl>
+                                <div className = "flex flex-row justify-end gap-1">
+                                    <Button className = "bg-gray-200/50 shadow-md rounded-sm aspect-square" ref = {confirmButtonRef} variant = "task" size = "icon" onClick ={() => {
+                                        form.handleSubmit(onSubmit);
+                                    }}>
+                                        <Check size = {24} strokeWidth = {2} color = "#52525B"></Check>
+                                    </Button>
+                                    <Button className = "bg-gray-200/50 shadow-md rounded-sm aspect-square" variant = "task" size = "icon">
+                                        <X size = {24} strokeWidth = {2} color = "#52525B"></X>
+                                    </Button>
+                                </div>
+                            </FormItem>
+                        )} />
+                    </form>
+                </Form>
             }
         </div>
     );
